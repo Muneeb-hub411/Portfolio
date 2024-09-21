859b581f21bdb7820b7d971c3bdaa4af636df4a9
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-09-21 16:14:23 +0500

    added new resume and modified about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,19 +10,25 @@ const About = () => {
         </h2>
       </div>
       <div className="m-4 mt-6">
-        <p className=" text-xl text-gray-500">
-          At the heart of our digital solutions is a passionate software
-          engineer with a specialization in web development. With two years of
-          invaluable experience collaborating with US-based companies, I have
-          successfully crafted numerous websites tailored to diverse client
-          needs. My expertise spans the full spectrum of web technologies,
-          enabling me to deliver robust, user-friendly, and scalable solutions.
-          Having worked on a variety of projects, I have honed my skills in
-          translating client visions into reality, always staying at the
-          forefront of industry trends. My commitment to excellence, coupled
-          with a deep understanding of both front-end and back-end development,
-          ensures that every project I undertake is executed with precision and
-          innovation.
+        <p className=" text-xl text-gray-500 leading-8">
+          I am a passionate software engineer with a strong background in web
+          development, having graduated from SS CASE IT in Islamabad. Over the
+          past two years, I have worked extensively as a junior web developer at
+          022 Marketing Agency and IT Verticals, where I played a pivotal role
+          in creating and managing a variety of websites tailored to different
+          client needs. My core expertise lies in the MERN stack and WordPress,
+          which allows me to develop full-stack applications and user-friendly
+          content management systems. From dynamic eCommerce platforms to
+          customized websites, I have honed my ability to translate business
+          requirements into functional, aesthetically pleasing digital
+          experiences. My experience working with US-based companies has exposed
+          me to global standards and the importance of delivering scalable,
+          high-performance solutions. Whether working on the front-end or
+          back-end, I always aim to innovate, staying at the forefront of
+          technology trends to ensure that every project I work on exceeds
+          expectations. My commitment to quality and my continuous learning
+          mindset drive me to solve complex problems with precision and
+          creativity.
         </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,26 @@
 import profilepic from "../assets/profileyellowbg.jpg";
-import resume from "../assets/MuneebSaleem_resume-2 (2).pdf";
+import resume from "../assets/Muneeb_Saleem_CV.pdf";
 const Home = () => {
   return (
     <div
       name="Home"
       className="  grid  md:grid-cols-2 md: w-full min-h-screen bg-gradient-to-b from-black to-gray-800"
     >
-      <div className="  h-[500px] flex flex-col flex-wrap justify-center items-center mt-20">
+      <div className="  h-[500px] flex flex-col flex-wrap justify-center items-center mt-20 ">
         <div className=" ">
           <div className="text-white font-oswald md:text-7xl md:m-4 xs:text-3xl xs:p-2 s8:text-4xl mt-20">
-            <h2 className="">Hello , I am a Web Developer</h2>
+            <h2 className="">I am a MERN Developer</h2>
           </div>
-          <div className="text-gray-500 text-xl m-4">
+          <div className="text-gray-500 text-xl m-4 leading-8">
             <p>
-              As a seasoned web developer and software engineer with over two
-              years of professional experience, I have honed my skills in
-              crafting robust, user-friendly digital solutions. My expertise
-              spans front-end and back-end technologies, allowing me to create
-              seamless, full-stack applications. I am passionate about
-              leveraging cutting-edge tools and methodologies to solve complex
-              problems and deliver high-quality software that exceeds client
-              expectations.
+              As a dedicated web developer with a strong foundation in software
+              engineering, I have accumulated two years of experience in web
+              development. Specializing in both the MERN stack and WordPress, I
+              have successfully built dynamic, user-centric applications that
+              balance functionality and design. I am passionate about utilizing
+              the latest technologies to develop efficient solutions,
+              continually delivering high-quality work that aligns with client
+              goals.
             </p>
             <button className=" p-4 font-semibold text-purple-800 rounded-md mt-4 hover:scale-110 transition[200] text-xl bg-gradient-to-r from-blue-400 to-blue-600">
               <a href={resume} download={true}>
